acad11ebce8a8b901ffcad7c6aac6990bc655218
Ryan Leite <>
2021-04-16 14:42:56 +0000

    atualização comando ping e adição comando steam

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,47 @@
-module.exports.run = async (client, message, args) => {
-  const m = await message.channel.send('ping?');
+const Discord = require("discord.js");
 
-  let embed = await new Discord.MessageEmbed()
-  (`🏓 **| Pong!**\nLatência do Server: **${m.createdTimestamp -
-      message.createdTimestamp}ms.**\nLatência da API: **${Math.round(
-      client.ws.ping
-    )}ms**`
-  );
-};
+exports.run = async(bot, message, args) => { // Aqui definimos nosso client, message e args
+
+    let embed = new Discord.MessageEmbed() // Aqui vai ser a primeira embed que o bot irá mostrar
+    .setTitle(message.author.username)
+    .setColor('RANDOM')
+    .setThumbnail(message.author.displayAvatarURL())
+    .setDescription(`Calculando...`)
+    .addField(`:white_check_mark: ⇝ Ajuda`, [
+        `Use \`!ajuda\` para saber mais comandos!`
+    ])
+
+    let embed2 = new Discord.MessageEmbed() // Aqui vai ser a segunda embed que o bot irá mostrar
+    .setTitle(message.author.username)
+    .setColor('RANDOM')
+    .setThumbnail(message.author.displayAvatarURL())
+    .setDescription("Espero que não esteja alto 😬!")
+    .addField(`:white_check_mark: ⇝ Ajuda`, [
+        `Use \`!ajuda\` para saber mais comandos!`
+    ])
+
+    let embed_ping = new Discord.MessageEmbed() // Aqui vai ser a terceira embed que o bot irá mostrar
+    .setTitle(message.author.username)
+    .setColor('RANDOM')
+    .setThumbnail(message.author.displayAvatarURL())
+    .addField(`:ping_pong: ⇝ Ping`,[
+        `\`${parseInt(bot.ws.ping)}\` ms\n`
+    ])
+    .addField(`:white_check_mark: ⇝ Ajuda`, [
+        `Use \`!ajuda\` para saber mais comandos!`
+    ])
+
+    const msg = await message.channel.send(embed) // Aqui o bot irá mostrar a primeira embed
+    setTimeout(() => { // Aqui criamos um timeout para mostrar a primeira embed com a duração de 3 segundos, para depois editar ela e mostrar a segunda embed
+      msg.edit(embed2)
+    }, 3000) // 1000 ms = 1s
+    setTimeout(() => { // Aqui criamos um timeout para mostrar a embed final com a duração de 5 segundos
+      msg.edit(embed_ping)
+    }, 5000)
+
+}
+
+exports.help = {
+    name: 'ping',
+    aliases: ["pingar"]
+}
